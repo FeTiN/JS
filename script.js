b57e9071727902e28340b781d3e312721074e660
VF <fetisovladimirr@gmail.com>
2022-03-14 17:14:44 +0200

    end array

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
 "use strict";
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function () {
-        console.log('Test');
-    }
-};
+const arr = [1, 12, 43, 26, 8];
 
-// Деструктуризация
+arr.sort(compareNum);
 
-    const  {border, bg} = options.colors;
-    console.log(border);
+function compareNum(a, b) {
+    return a - b;
+}
 
-// Деструктуризация
+console.log(arr);
 
-// options.makeTest();
+// arr.pop(); // удаление последний эл. с конца массива
+// arr.push(10); // добавление элемента в конец массива
+// arr.shift(); // удаляет из массива первый элемент и возвращает его
+// arr.unshift('a'); // добавляет элемент в начало массива
+// arr.split(s); // превращает строку в массив, s - разделитель
+// arr.join(s); // превращает массив в сроку, s - разделитель
+// delete arr[1]; // Удаляет второй элемент
+// arr.splice(index, count, elem1...); // удалить count элементов, начиная с index и заменить на элементы elem1
+// arr.slice(begin, end); // копирует часть массива с begin до end не включая
+// arr.sort(fn); // сортировка массива. Если не передать функцию сравнения - сортирует элементы как строки
+// arr.reverse(); // меняют порядок элементов на обратный
+// arr.concat(item1...); // создает новый массив, в который копируются элемениы из arr, а также item1...
 
-// Перебор ключ значение 
-// console.log(Object.keys(options).length);
 
-// console.log(options.name);
+// Методы перебора
 
-// delete options.name;
+// arr.forEach
+// arr.map
+// arr.every/some
+// arr.filter
+// arr.reduce
 
-// console.log(options);
 
-// for ( let key in options ) {
-//     if (typeof(options[key]) === 'object') {
-//         for ( let i in options[key] ) {
-//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//         }
-//     } else {
-//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//     }
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
 // }
+
+// for (let j of arr) {
+//     console.log(j);
+// }
+
+// arr.forEach(function(item, i, arr){
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// const str = prompt('', '');
+// const products = str.split(', ');
+// products.sort();
+// console.log(products.join('; '));
