56d5ff76491394342e4f0d73be288af3b91ce56e
VladimirFetisov <vladimir@one-v.co.il>
2022-04-11 17:12:15 +0300

    tasts 1

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
+/*
+    Какое будет выведено значение: let x = 5; alert( x++ ); ?
+    Чему равно такое выражение: [ ] + false - null + true ?
+    Что выведет этот код: let y = 1; let x = y = 2; alert(x); ?
+    Чему равна сумма [ ] + 1 + 2?
+    Что выведет этот код: alert( &quot;1&quot;[0] )?
+    Чему равно 2 &amp;&amp; 1 &amp;&amp; null &amp;&amp; 0 &amp;&amp; undefined ?
+    Есть ли разница между выражениями? !!( a &amp;&amp; b ) и (a &amp;&amp; b)?
+    Что выведет этот код: alert( null || 2 &amp;&amp; 3 || 4 ); ?
+    a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ?
+    Что выведет этот код: alert( +&quot;Infinity&quot; ); ?
+    Верно ли сравнение: &quot;Ёжик&quot; &gt; &quot;яблоко&quot;?
+    Чему равно 0 || &quot;&quot; || 2 || undefined || true || falsе ?
+*/
+
 'use strict';
 
-// 1.
-console.log(typeof(String(null)));
-console.log(typeof(String(4)));
+// let x = 5;
+// console.log(x++); - 5
 
-// 2.
-console.log(typeof(null + ''));
+// console.log([ ] + false - null + true); - NaN
 
-const num = 5;
-console.log("https://vk.com/catalog/" + num );
+// let y = 1; 
+// let x = y = 2;
 
-const fontSize = 26 + 'px';
+// console.log(x); - 2
 
-// To Number
+// console.log([] + 1 + 2); = "12"
 
-// 1.
-console.log(typeof(Number('4')));
+// console.log( "1"[0] ); - 1
 
-// 2.
-console.log(typeof(+'5'));
+// console.log(2 && 1 && null && undefined); = null
 
-// 3.
-console.log(typeof(parseInt("15px", 10)));
+// console.log( !!(a && b) и (a && b) ); = false
 
-// To Boolean
+// console.log(null || 2 && 3 || 4); = 3
 
-// 0, '', null, undefined, NaN
+// a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ?  false
 
-let switcher = null;
+// Что выведет этот код: alert( +"Infinity" ); ?  infinity
 
-if (switcher) {
-    console.log('Working...');
-}
+// console.log("Ёжик" > "яблоко"); false
 
-switcher = 1;
-
-if (switcher) {
-    console.log('Working...');
-}
-
-
-console.log(typeof(Boolean('4')));
-
-console.log(typeof(!!"44444"));
+// console.log(0 || "" || 2 || undefined || true || falsе);  2
